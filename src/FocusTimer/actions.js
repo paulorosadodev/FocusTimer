45bb539c3c33ec6b619e56a5636adf02f84d5499
Paulo Rosado <paulohenrique110803@gmail.com>
2024-07-12 19:34:00 -0300

    setting actions

---
+++ src/FocusTimer/actions.js
@@ -0,0 +1,15 @@
+export function toggleRunning() {
+    document.documentElement.classList.toggle('running')
+}
+
+export function set() {
+
+}
+
+export function reset() {
+
+}
+
+export function toggleMusic() {
+    
+}
